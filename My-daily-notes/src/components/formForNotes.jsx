5a53432bd1782cac70f3b78f5b5e79b0e750afea
GriseldaAlonso <gri_alonso@hotmail.com>
2023-02-20 11:54:20 -0500

    Agregando campo de timeStamp al almacenamiento de notas

--- My-daily-notes/src/components/formForNotes.jsx
+++ My-daily-notes/src/components/formForNotes.jsx
@@ -4,17 +4,20 @@ import { useUserContext } from '../context/userContext'
 export function FormForNotes () {
 
   const {saveNotes} = useUserContext();
+  const ts = new Date();
+  const currentDate = ts.toLocaleString();
 
   const initialValue = {
     title: '',
-    text: ''
+    text: '',
+    ts: '',
   };
 
   const [user, setUser] = useState(initialValue);
 
   const dataCapture = (e) => {
     const {name, value} = e.target;
-    setUser({...user, [name]:value})
+    setUser({...user, [name]:value, ts: currentDate})
   }
 
   /**
